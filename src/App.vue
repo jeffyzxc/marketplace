b17f787218da9260444f5bd3ba9009e44d1a589a
Nixon Galario <nixongalario@gmail.com>
2021-08-07 03:08:45 +0800

    basic scaffolding of layout and routing

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,31 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div class="app">
+    <b-container class="bv-example-row" fluid>
+      <b-row class="navbar-cb-logo">
+        <b-col cols="3" class="logo-title">CryptoBlades</b-col>
+        <b-col cols="9" class="nav-top">
+          <div class="nav">
+            <router-link to="/">Buy</router-link>
+            <router-link to="/sell">Sell</router-link>
+            <router-link to="/listings">My listings</router-link>
+            <router-link to="/history">Transaction History</router-link>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
+    <router-view class="switch-view-container"/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<style scoped>
+.switch-view-container {
+  height:100%;
+}
+.navbar-cb-logo {
+  height : 3.5rem;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.logo-title, .nav-top {
+  border: 1px solid rgba(57, 67, 85, 1);
 }
 </style>
---
+++ src/views/Buy.vue
@@ -0,0 +1,35 @@
+<template>
+  <b-container class="bv-example-row" fluid>
+      <b-row class="buy-main">
+        <b-col cols="3" class="market-filters">Market Filters</b-col>
+        <b-col cols="9">
+          <div>
+            Banner Section
+          </div>
+          <div>
+            Filter Section
+          </div>
+          <div>
+            Listing all Section
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Buy'
+});
+</script>
+
+<style>
+.buy-main {
+  height:100%;
+}
+.market-filters {
+  border-right: 1px solid rgba(57, 67, 85, 1);
+}
+</style>
