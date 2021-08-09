9601c367ea8a767c5675e21e81b5547a4b3ba98e
Nixon Galario <nixongalario@gmail.com>
2021-08-09 13:25:02 +0800

    navbar completion

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,24 @@
             <router-link to="/listings"  class="navlinks" active-class="active-navlink">My listings</router-link>
             <router-link to="/history"  class="navlinks" active-class="active-navlink">Transaction History</router-link>
           </div>
+          <div class="wallet-details">
+            <div class="skill-wallet">
+              <img src="./assets/addButton.png" alt="" srcset="" class="add-wallet-icon">
+              <span>Buy Skill</span>
+            </div>
+            <div class="skill-wallet">
+              <img src="./assets/skillicon.png" alt="" srcset="" class="add-wallet-icon">
+              <span>50.299</span>
+            </div>
+            <div class="bnb-wallet">
+              <img src="./assets/binance-coin-logo@2x.png" alt="" srcset="" class="add-wallet-icon">
+              <span>10.299</span>
+            </div>
+
+            <div class="wallet-address">
+              <span>0x2B…61cB</span>
+            </div>
+          </div>
         </b-col>
       </b-row>
     </b-container>
@@ -17,7 +35,17 @@
   </div>
 </template>
 
+<style>
+html, body {
+  margin:0px;
+  height:100%;
+}
+</style>
 <style scoped>
+.app {
+  background-color: rgb(8,20,43);
+  height:100%;
+}
 .switch-view-container {
   height:100%;
 }
@@ -45,9 +73,44 @@
 }
 .nav-top > .nav {
   align-items: center;
+  justify-self: flex-start;
+  flex : 2
 }
 
 .active-navlink {
   color : rgb(255, 255, 255);
+}
+
+.wallet-details {
+  flex : 1;
+  justify-self: flex-end;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+}
+
+.add-wallet-icon {
+  height: 1rem;
+}
+
+.wallet-address {
+  width: 141px;
+  height: 38px;
+  background: #253148 0% 0% no-repeat padding-box;
+  border-radius: 5px;
+  opacity: 1;
+  text-align: center;
+  color: #FFFFFF;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.skill-wallet > span, .bnb-wallet > span {
+  margin-left: 5px;
+}
+
+.wallet-details > * {
+  color : white;
 }
 </style>
