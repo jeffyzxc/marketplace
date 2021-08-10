847e9ff468297dbfdc3f0d7a4c3bed0e2f40193e
Nixon Galario <nixongalario@gmail.com>
2021-08-11 02:17:22 +0800

    added weapon list

---
+++ src/components/WeaponItem.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="item-card" style="border: 1px solid #810FD1;">
+    <div class="imgs"><div class="label">
+        <div><p>Unique</p><p>LB:30/100</p></div>
+        <div><span>#9761</span></div></div>
+        <img class="weapons" src="../assets/weapons/weapon1.png" alt="">
+        </div>
+        <div class="desc d-flex justify-content-start align-items-center">
+            <img width="20" src="../assets/nav-icons/fire.png" alt=""><span>INFUSED SWORD</span></div>
+            <div class="substat"><div class="stats"><img width="11" src="../assets/nav-icons/water.png" alt=""><span>+395</span></div>
+            <div class="stats"><img width="11" src="../assets/nav-icons/fire.png" alt=""><span>+395</span></div><div class="stats">
+                <img width="11" src="../assets/nav-icons/earth.png" alt=""><span>+395</span></div><div class="stats">
+                    <img width="11" src="../assets/nav-icons/lightning.png" alt=""><span>+395</span></div></div><div class="cost-item"><div><img width="15" src="Images/header-icons/apple-touch-icon.png" alt="">&nbsp;<span>5.13</span></div><div><p class="btn-purchase csr-pointer">Purchase</p></div>
+        </div></div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+    name: 'SortFilter'
+});
+
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Buy.vue
+++ src/views/Buy.vue
@@ -2,16 +2,18 @@
   <b-container class="bv-example-row" fluid>
       <b-row class="buy-main">
         <b-col cols="2" class="market-filters"><market-filter/></b-col>
-        <b-col cols="10">
-          <div>
-            Banner Section
+        <b-col cols="10" class="bdr-line-left pr-5 item-list ps-container ps-active-y">
+          <div class="banner-ads pt-4">
+            <div class="bg-banner"></div>
           </div>
           <div>
-            Filter Section
+            <sort-filter />
           </div>
-          <div>
-            Listing all Section
-          </div>
+           <div class="list-items">
+                <div class="spacer flex-wrap d-flex append-weapon">
+                  <weapon-item/>
+                </div>
+            </div>
         </b-col>
       </b-row>
     </b-container>
@@ -19,11 +21,15 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import MarketFilter from '../components/marketFilter.vue'
+import MarketFilter from '../components/marketFilter.vue';
+import SortFilter from '../components/SortFilter.vue';
+import WeaponItem from '../components/WeaponItem.vue';
 export default Vue.extend({
   name: 'Buy',
   components: {
-        MarketFilter
+        MarketFilter,
+        SortFilter,
+        WeaponItem
     }
 });
 </script>
