677a6dce7d8c5151de0736c4cb0e7b97cdfbb716
Nixon Galario <nixongalario@Nixons-MacBook-Pro.local>
2021-08-28 13:31:18 +0800

    fetch api working with element

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,32 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import { BASE_API_URL } from '../const/environments';
 Vue.use(Vuex)
+
+interface IWeapon {
+  id:            string;
+  price:         number;
+  weaponID:      string;
+  weaponStars:   number;
+  weaponElement: string;
+  stat1Element:  string;
+  stat1Value:    number;
+  stat2Element:  string;
+  stat2Value:    number;
+  stat3Element:  string;
+  stat3Value:    number;
+  timestamp:     number;
+  sellerAddress: string;
+  buyerAddress:  null;
+  network:       string;
+}
 
 export const store = new Vuex.Store({
   state: {
     defaultAccount: '',
     chainId: '',
     metamaskConnected: false,
+    weaponsList : []
   },
   mutations: {
     setDefaultAaccount (state, payload) {
@@ -18,14 +37,29 @@ export const store = new Vuex.Store({
     },
     setMetamaskConnected (state, payload) {
         state.metamaskConnected = payload;
-      }
+    },
+    setWeaponsList: (state, weapons) => (state.weaponsList = weapons),
+    getWeaponsList: function(state, payload) {
+      state.weaponsList = payload
+    },
   },
   actions: {
+    async fetchWeaponsList({ commit }) {
+      try {
+          const response = await fetch(`${BASE_API_URL}/static/market/weapon?pageSize=20?pageNum=0`);
+          const data = await response.json();
+          console.log('data', data.results);
+          commit('setWeaponsList', data.results);
+      } catch (error) {
+          console.error(error);
+      }
+  },
   },
   modules: {
   },
   getters : {
       getMetamaskConnected : state => state.metamaskConnected,
-      defaultAccount : state => state.defaultAccount
+      defaultAccount : state => state.defaultAccount,
+      allWeapons: (state) => state.weaponsList,
   }
 })
